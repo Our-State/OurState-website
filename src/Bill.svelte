4a6cdd21e2627ed7b4ce64135bd00ab1fc04ab36
YutYouGit <yutyoulook@outlook.com>
2023-07-31 17:11:29 -0400

    messasge

--- src/Bill.svelte
+++ src/Bill.svelte
@@ -4,7 +4,6 @@
 
 <main>
 <!--TODO, make an input system for this and clean up the code-->
-<button on:click={myFunction}>Click here!!!!!!!!!!!</button>
 <br>
 <br>
 <span>Please enter your bill's name:</span>
@@ -12,32 +11,47 @@
 <input type="text" id="title" placeholder="Title">
 <br>
 <br>
-<span>Please enter your bill's name:</span>
+<span>Why do you want to pass this bill:</span>
 <br>
-<input type="text" id="title" placeholder="Title">
+<input type="text" id="reasoning" placeholder="Reasoning">
+<br>
+<br>
+<span>What is the date you want to pass this bill by? In [Month],[Day],[Year] format (optional):</span>
+<br>
+<input type="text" id="Date" placeholder="Date">
+<br>
+<br>
+<button on:click={myFunction}>Click Here to Submit</button>
 
 
 <p id="welovecookies"></p>
 </main>
-<script>
+<script lang="ts">
+
 function myFunction() {
-  let title = prompt("Please enter your bill's name", "");
-  let reasoning = prompt("Why do you want to pass this bill (optional)", "");
-  let date = prompt("What is the date you want to pass this bill by? In [Month],[Day],[Year] format (optional)", "January 1st, 2023");
-
+  let title = (
+      document.getElementById("title") as HTMLInputElement
+    );
+  let reasoning = (
+      document.getElementById("reasoning") as HTMLInputElement
+    );
+  let date = (
+      document.getElementById("date") as HTMLInputElement
+    );
  
-  if (reasoning != null) {
-
-    document.getElementById("welovecookies").innerHTML =
-    "Write me a proposed bill for the New Jersey legislature titled " + title + " along with 3 clauses: one clause that explains why it is important and " + reasoning + ", one clause with the actions it will take to achieve it, and one enactment clause under the previous clause that clearly explains the actions that will be made and states the date that it will be passed: " + date;
-
-
-  } else {
-
+  if (reasoning!=null && title!=null) {
+    if (date=null) {
     document.getElementById("welovecookies").innerHTML =
     "Write me a proposed bill for the New Jersey legislature titled " + title + " along with 3 clauses: one clause that explains why it is important, one clause with the actions it will take to achieve it, and one enactment clause under the previous clause that clearly explains the actions that will be made and states the date that it will be passed";
-
+    } else if (date!=null) {
+    document.getElementById("welovecookies").innerHTML =
+    "Write me a proposed bill for the New Jersey legislature titled " + title + " along with 3 clauses: one clause that explains why it is important and " + reasoning + ", one clause with the actions it will take to achieve it, and one enactment clause under the previous clause that clearly explains the actions that will be made and states the date that it will be passed: " + date;
+    } 
+  } else {
+    document.getElementById("welovecookies").innerHTML = "Please fill in the reasoning or the title"
+    
   }
 }
+
 </script>
 
